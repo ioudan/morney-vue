380ccd0433bb4bbe494824a896540c25a976e5e3
ioudan <2020.meng.dd>
2021-01-22 16:22:49 +0800

    deploy

---
+++ precache-manifest.5013c799283cdc680d33be07d1a40503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c59c4fd16356e9b113",
+    "url": "/morney-vue/css/app.f2f289a9.css"
+  },
+  {
+    "revision": "72f7f1fa2fcb6f49ade0d29c2a0f77ae",
+    "url": "/morney-vue/index.html"
+  },
+  {
+    "revision": "f2c59c4fd16356e9b113",
+    "url": "/morney-vue/js/app.1da9fa68.js"
+  },
+  {
+    "revision": "b964811b240d56ea56cf",
+    "url": "/morney-vue/js/chunk-vendors.84d8bd55.js"
+  },
+  {
+    "revision": "4b700bda4a3f05b842705dfbb2508bee",
+    "url": "/morney-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-vue/robots.txt"
+  }
+]);
